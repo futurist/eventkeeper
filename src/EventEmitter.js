d09fd7c84da5daff054633e2714590bbf162a99c
Thomas Mosey <tom@thomasmosey.com>
2015-05-13 12:16:40 +0100

    Added transpiling

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -78,11 +78,4 @@ class EventEmitter {
 
 }
 
-/* Unfortunately I have to support IE8. So there's that. */
-if(typeof Array.isArray === 'undefined') {
-	Array.isArray = function(obj) {
-		return Object.prototype.toString.call(obj) === '[object Array]';
-	}
-}
-
-module.exports = EventEmitter;
+export default EventEmitter;
